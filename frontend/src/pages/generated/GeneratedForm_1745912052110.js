029a2b647272976edc1035a0600a44e54546ca82
Malkood <MALKORE1@Alcon.net>
2025-04-29 16:26:03 +0530

    Second changes 29/04

---
+++ frontend/src/pages/generated/GeneratedForm_1745912052110.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const GeneratedForm = () => {
+  return (
+    <div className="p-6 bg-white text-black min-h-screen">
+      <h1 className="text-2xl font-bold mb-6">Generated Form</h1>
+      <form className="flex flex-col gap-4">
+        
+        <label>Field</label>
+        <input type="text" className="p-2 border rounded mb-2" />
+      
+
+        <label>Field</label>
+        <input type="text" className="p-2 border rounded mb-2" />
+      
+      </form>
+    </div>
+  );
+};
+
+export default GeneratedForm;
